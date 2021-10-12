751f1f2104c44e6c1219e54fd89c0e1c4a486f6d
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:25:08 -0700

    SystemUI: Add dual-tone light and dark themes for QS
    Google's dual-tone QS design where the notification panel has a
    semantically higher elevation adds depth to the notification+QS shade,
    and we don't necessarily have to give it up just because our QS has
    light and dark themes.
    
    To preserve the dual-tone effect, use a darker background color for the
    QS section:
    
    Light:
        Notifications: neutral1 20 (surface_light)
        Notification panel: neutral1 50 (light BG)
        QS background: neutral1 100 (darker light BG / surface_header_light)
        Inactive QS tiles: neutral1 20 (surface_light)
    
    Dark:
        Notifications: neutral1 800 (surface_dark)
        Notification panel: neutral1 900 (dark BG)
        QS background: neutral1 950 (surface_header_dark_sysui modulated to L* 5)
        Inactive QS tiles: neutral1 800 (surface_dark)
    
    The dark QS background could be neutral1 0 (black) like it was before,
    but I don't think it looks as good because it can't be tinted based on
    the active wallpaper and thus stands out from other colors.
    
    Unfortunately, Google's current CAM16-based modulation causes hue shifts
    in extreme light and dark shades (e.g. L* = 98 / 5), but we'll fix this
    by generating and overlaying modulated surface colors in our
    ThemeOverlayController implementation.
    
    Demo screenshots: https://twitter.com/kdrag0n/status/1445922541218922496
    
    Change-Id: Icdc4957ecb4e0201377351f1a3e1c6928d6b3955
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -229,6 +229,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     private final KeyguardInteractor mKeyguardInteractor;
 
     private GradientColors mColors;
+    private GradientColors mBehindColors;
     private boolean mNeedsDrawableColorUpdate;
 
     private float mAdditionalScrimBehindAlphaKeyguard = 0f;
@@ -398,6 +399,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         mKeyguardInteractor = keyguardInteractor;
         mWallpaperRepository = wallpaperRepository;
         mMainDispatcher = mainDispatcher;
+        mBehindColors = new GradientColors();
     }
 
     @Override
@@ -1261,7 +1263,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
                     && !mBlankScreen;
 
             mScrimInFront.setColors(mColors, animateScrimInFront);
-            mScrimBehind.setColors(mColors, animateBehindScrim);
+            mScrimBehind.setColors(mBehindColors, animateBehindScrim);
             mNotificationsScrim.setColors(mColors, animateScrimNotifications);
 
             dispatchBackScrimState(mScrimBehind.getViewAlpha());
@@ -1613,8 +1615,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         if (mScrimBehind == null) return;
         int background = Utils.getColorAttr(mScrimBehind.getContext(),
                 com.android.internal.R.attr.materialColorSurfaceDim).getDefaultColor();
+        int surfaceBackground = Utils.getColorAttr(mScrimBehind.getContext(),
+                com.android.internal.R.attr.colorSurfaceHeader).getDefaultColor();
         int accent = Utils.getColorAttr(mScrimBehind.getContext(),
                 com.android.internal.R.attr.materialColorPrimary).getDefaultColor();
+
         mColors.setMainColor(background);
         mColors.setSecondaryColor(accent);
         final boolean isBackgroundLight = !ContrastColorUtil.isColorDark(background);
@@ -1625,6 +1630,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         for (ScrimState state : ScrimState.values()) {
             state.setSurfaceColor(surface);
         }
+
+        mBehindColors.setMainColor(surfaceBackground);
+        mBehindColors.setSecondaryColor(accent);
+        mBehindColors.setSupportsDarkText(
+                ColorUtils.calculateContrast(mBehindColors.getMainColor(), Color.WHITE) > 4.5);
 
         mNeedsDrawableColorUpdate = true;
     }
