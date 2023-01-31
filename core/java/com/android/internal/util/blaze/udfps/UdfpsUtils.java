53f1b43d7f5011da75a6256e4164c1c74a07e055
Joey Huab <joey@evolution-x.org>
2023-01-31 08:42:46 +0000

    UdfpsUtils: Use config_supportsUdfps to check for all Udfps devices
    * Set to false by default.

--- core/java/com/android/internal/util/blaze/udfps/UdfpsUtils.java
+++ core/java/com/android/internal/util/blaze/udfps/UdfpsUtils.java
@@ -20,6 +20,8 @@ public class UdfpsUtils {
     public static boolean hasUdfpsSupport(Context context) {
         int[] udfpsProps = context.getResources().getIntArray(
                 com.android.internal.R.array.config_udfps_sensor_props);
-        return !ArrayUtils.isEmpty(udfpsProps);
+
+        return (!ArrayUtils.isEmpty(udfpsProps)
+                || context.getResources().getBoolean(com.android.internal.R.bool.config_supportsUdfps));
     }
 }
