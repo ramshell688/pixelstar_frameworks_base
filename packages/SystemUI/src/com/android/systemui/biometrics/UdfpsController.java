Evolution-X-AOSP/frameworks_base

base: Rename UdfpsUtils -> CustomUdfpsUtils

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -70,7 +70,7 @@ import androidx.annotation.Nullable;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
-import com.android.internal.util.evolution.udfps.UdfpsUtils;
+import com.android.internal.util.evolution.udfps.CustomUdfpsUtils;
 import com.android.keyguard.FaceAuthApiRequestReason;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.settingslib.udfps.UdfpsOverlayParams;
@@ -963,7 +963,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
         mUdfpsVendorCode = mContext.getResources().getInteger(R.integer.config_udfpsVendorCode);
-        mDisableNightMode = UdfpsUtils.hasUdfpsSupport(mContext);
+        mDisableNightMode = CustomUdfpsUtils.hasUdfpsSupport(mContext);
 
         mAmbientDisplayConfiguration = new AmbientDisplayConfiguration(mContext);
         boolean screenOffFodSupported = mContext.getResources().getBoolean(

ramshell688/pixelstar_frameworks_base

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -73,7 +73,7 @@ import androidx.annotation.OptIn;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.logging.InstanceId;
 import com.android.internal.util.LatencyTracker;
-import com.android.internal.util.blaze.udfps.UdfpsUtils;
+import com.android.internal.util.blaze.udfps.CustomUdfpsUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.Dumpable;
 import com.android.systemui.R;
@@ -855,7 +855,7 @@ public class UdfpsController implements DozeReceiver, Dumpable {
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
         mUdfpsVendorCode = mContext.getResources().getInteger(R.integer.config_udfpsVendorCode);
-        mDisableNightMode = UdfpsUtils.hasUdfpsSupport(mContext);
+        mDisableNightMode = CustomUdfpsUtils.hasUdfpsSupport(mContext);
 
         mAmbientDisplayConfiguration = new AmbientDisplayConfiguration(mContext);
         boolean screenOffFodSupported = mContext.getResources().getBoolean(
49ec6452e10c3096a2e2a30dbc19bba6dc165247
Joey Huab <joey@evolution-x.org>
2023-10-24 02:18:49 +0900

    base: Rename UdfpsUtils -> CustomUdfpsUtils

--- core/java/com/android/internal/util/blaze/udfps/CustomUdfpsUtils.java
+++ core/java/com/android/internal/util/blaze/udfps/CustomUdfpsUtils.java
@@ -16,7 +16,7 @@
 package com.android.internal.util.blaze.udfps;
 import android.content.Context;
 import com.android.internal.util.ArrayUtils;
-public class UdfpsUtils {
+public class CustomUdfpsUtils {
     public static boolean hasUdfpsSupport(Context context) {
         int[] udfpsProps = context.getResources().getIntArray(
                 com.android.internal.R.array.config_udfps_sensor_props);
